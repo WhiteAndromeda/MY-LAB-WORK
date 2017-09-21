e08e98f1f8b2b0a0512450d3d5aad171a69c15c0
Nikita <31614450+WhiteAndromeda@users.noreply.github.com>
2017-09-21 23:25:59 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,54 +1,112 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#define K0 273.15
+#define K1 459.67
 
-int main()
+float t;    //temperature
+char scale;    //temperature scales
+
+int main(int argc, char *argv[])    //counting the number of variables
 {
-    double t = 5;
-    char scale;
-    #define T0 273.15
-    scale = 'K';
+    if (argc < 2)
+    {
+        printf("No input data");
+        exit(0);
+    }
+    else
+    {
+        printf("$ tconvert ");
+        if (argc > 2)
+        {
+            t = atof(argv[1]);
+            scale = toupper(atof(argv[2]));
+        }
+        else
+            t = atof(argv[1]);
+    }
+    
 
-    printf("$ tconvert ");
-    scanf("%.2f%c", &t, &scale);
     switch(scale)
     {
-        case 'C' :
+        case 'C' :    //celsius
         {
-        	printf("%.2f C\n", t);
-        	printf("%.2f F\n", t);
-        	printf("%.2f K", t);
-            break;
+            if (t < -K0)
+            {
+                printf("invalid input");
+                exit(0);
+            }
+            else
+            {
+                printf("%.2f C\n", t);
+                printf("%.2f F\n", (t*9/5+32));
+        	    printf("%.2f K", (t+K0));
+                break;
+            }
 		}
-		case 'F' :
+		case 'F' :    //fahrenheit
 		{
-        	printf("%.2f F\n", t);
-        	printf("%.2f C\n", t);
-        	printf("%.2f K", t);
-        	break;
+            if (t < -K1)
+            {
+                printf("invalid input");
+                exit(0);
+            }
+            else
+            {
+        	    printf("%.2f F\n", t);
+        	    printf("%.2f C\n", ((t-32)*5/9));
+        	    printf("%.2f K", ((t+K1)*5/9));
+                break;
+            }
 		}
-		case 'K' :
+		case 'K' :    //kelvin
 		{
-        	printf("%.2f K\n", t);
-        	printf("%.2f C\n", t);
-        	printf("%.2f F", t);
-        	break;
+            if (t < 0)
+            {
+                printf("invalid input");
+                exit(0);
+            }
+            else
+            {
+        	    printf("%.2f K\n", t);
+        	    printf("%.2f C\n", (t-K0));
+                printf("%.2f F", (t*9/5-K1));
+                break;
+            }
         }
         default :
         {
-        	printf("%.2f\n",t);
+        	printf("%.2f\n\n", t);
 
-        	printf("%.2f C:\n",t);
-        	printf("%.2f F\n",t);
-        	printf("%.2f K\n\n",t);
+            if (t < -K0)
+                printf("invalid input\n\n");
+            else
+            {
+        	    printf("%.2f C:\n", t);
+        	    printf("%.2f F\n", (t*9/5+32));
+        	    printf("%.2f K\n\n", (t+K0));
+            }
+            if (t < -K1)
+                printf("invalid input\n\n");
+            else
+            {
+        	    printf("%.2f F:\n", t);
+        	    printf("%.2f C\n", ((t-32)*5/9));
+        	    printf("%.2f K\n\n", ((t+K1)*5/9));
+            }
 
-        	printf("%.2f F:\n",t);
-        	printf("%.2f C\n",t);
-        	printf("%.2f K\n\n",t);
-
-        	printf("%.2f K:\n",t);
-        	printf("%.2f C\n",t);
-        	printf("%.2f F",t);
-        	break;
+            if (t < 0)
+            {
+                printf("invalid input\n\n");
+                exit(0);
+            }
+            else
+            {
+        	    printf("%.2f K:\n", t);
+        	    printf("%.2f C\n", (t-K0));
+        	    printf("%.2f F", (t*9/5-K1));
+                break;
+            }
 		}
 
     }
