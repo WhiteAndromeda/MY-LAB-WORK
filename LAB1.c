518d7304b118e666608f8c712ec18032be316113
Nikita <31614450+WhiteAndromeda@users.noreply.github.com>
2017-09-23 22:02:55 +0300

    Update LAB1.c

--- LAB1.c
+++ LAB1.c
@@ -1,124 +1,76 @@
+//pikbo_01_17_0943_1
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-float t;    //temperature
-char scale;    //temperature scales
-float min[] = {0, 273.15, 459.67};
+short int Flag = 0;
+float temperature;
+char *scales[]={"K", "C", "F"};
+float min[]={0.0, 273.15, 459.67};
 
-int main(int argc, char *argv[])    //counting the number of variables
+float * converter(float t, int num)
+{
+    if (t < -min[num])
+    {
+        printf("\n%.2f %s:\nThis temperature does not exist\n", t, scales[num]);
+        return NULL;
+    }
+    else
+        if (Flag == 0)
+        {
+            Flag = 1;
+            printf("\n$ tconvert ");
+        }
+
+    switch(num)
+    {
+        case 0: 
+            printf("%.2f K:\n", t);
+            printf("%.2f C\n", (t - min[1]));
+            printf("%.2f F\n\n", (t * 9.0 / 5.0 - min[2]));
+            break;
+        case 1: 
+            printf("%.2f C:\n", t);
+            printf("%.2f F\n", (t * 9.0 / 5.0 + 32.0));
+            printf("%.2f K\n\n", (t + min[1]));
+            break;
+        case 2:
+            printf("%.2f F:\n", t);
+            printf("%.2f C\n", ((t - 32.0) * 5.0 / 9.0));
+            printf("%.2f K\n", ((t + min[2])* 5.0 / 9.0));
+            break;
+    }
+}
+
+int main(int argc, char *argv[])  
 {
     switch(argc)
     {
-        case 1 :
+        case 1:
         {
-            printf("No input data\n");ы
-            return 0;
-        }
-        case 2 :
-            t = atof(argv[1]);
+            printf("\nNo input data\n");
             break;
-        case 3 :
+        }
+        case 2:
+        {
+            temperature = atof(argv[1]);
+            for (int i = 0; i <= 2; ++i)
+                converter(temperature, i);
+            break;
+        }
+        case 3:
         {   
-            if (stricmp("C", argv[2])== 0)
-                scale = 'C';
-            else
-                if(stricmp("F", argv[2])== 0)
-                    scale = 'F';
-                else
-                    if(stricmp("K", argv[2])== 0)
-                        scale = 'K';
-            t = atof(argv[1]);
+            temperature = atof(argv[1]);
+            for (int i = 0; i <= 2; ++i)
+                if (stricmp(scales[i], argv[2]) == 0)
+                {
+                    converter(temperature, i);
+                    return 0;
+                }
+            for (int i = 0; i <= 2; ++i)
+                converter(temperature, i);
             break;
-        default:
-            ;
         }
     }
-
-    printf("$ tconvert ");
-
-    switch(scale)
-    {
-        case 'C' :    //celsius
-        {
-            if (t < -min[1])
-            {
-                printf("invalid input");
-                exit(0);
-            }
-            else
-            {
-                printf("%.2f C\n", t);
-                printf("%.2f F\n", (t*9/5+32));
-        	    printf("%.2f K\n", (t+min[1]));
-                break;
-            }
-		}
-		case 'F' :    //fahrenheit
-		{
-            if (t < -min[2])
-            {
-                printf("invalid input");
-                exit(0);
-            }
-            else
-            {
-        	    printf("%.2f F\n", t);
-        	    printf("%.2f C\n", ((t-32)*5/9));
-        	    printf("%.2f K\n", ((t+min[2])*5/9));
-                break;
-            }
-		}
-		case 'K' :    //kelvin
-		{
-            if (t < min[0])
-            {
-                printf("invalid input");
-                exit(0);
-            }
-            else
-            {
-        	    printf("%.2f K\n", t);
-        	    printf("%.2f C\n", (t-min[1]));
-                printf("%.2f F\n", (t*9/5-min[2]));
-                break;
-            }
-        }
-        default :
-        {
-        	printf("%.2f\n\n", t);
-
-            if (t < -min[1])
-                printf("invalid input\n\n");
-            else
-            {
-        	    printf("%.2f C:\n", t);
-        	    printf("%.2f F\n", (t*9/5+32));
-        	    printf("%.2f K\n\n", (t+min[1]));
-            }
-            if (t < -min[2])
-                printf("invalid input\n\n");
-            else
-            {
-        	    printf("%.2f F:\n", t);
-        	    printf("%.2f C\n", ((t-32)*5/9));
-        	    printf("%.2f K\n\n", ((t+min[2])*5/9));
-            }
-
-            if (t < min[0])
-            {
-                printf("invalid input\n\n");
-                exit(0);
-            }
-            else
-            {
-        	    printf("%.2f K:\n", t);
-        	    printf("%.2f C\n", (t-min[1]));
-        	    printf("%.2f F\n", (t*9/5-min[2]));
-            }
-		}
-
-    }
-
     return 0;
 }
