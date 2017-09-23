774fc8b38e7449d350db8fdd5b27c4a503edd9ef
Nikita <31614450+WhiteAndromeda@users.noreply.github.com>
2017-09-23 06:35:23 +0300

    Update LAB1.c

--- LAB1.c
+++ LAB1.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define K0 273.15
-#define K1 459.67
 
 float t;    //temperature
 char scale;    //temperature scales
+float min[] = {0, 273.15, 459.67};
 
 int main(int argc, char *argv[])    //counting the number of variables
 {
@@ -13,21 +12,21 @@ int main(int argc, char *argv[])    //counting the number of variables
     {
         case 1 :
         {
-            printf("No input data");
-            exit(0);
+            printf("No input data\n");ы
+            return 0;
         }
         case 2 :
             t = atof(argv[1]);
             break;
         case 3 :
         {   
-            if (strcmp("C", argv[2])== 0)
+            if (stricmp("C", argv[2])== 0)
                 scale = 'C';
             else
-                if(strcmp("F", argv[2])== 0)
+                if(stricmp("F", argv[2])== 0)
                     scale = 'F';
                 else
-                    if(strcmp("K", argv[2])== 0)
+                    if(stricmp("K", argv[2])== 0)
                         scale = 'K';
             t = atof(argv[1]);
             break;
@@ -42,7 +41,7 @@ int main(int argc, char *argv[])    //counting the number of variables
     {
         case 'C' :    //celsius
         {
-            if (t < -K0)
+            if (t < -min[1])
             {
                 printf("invalid input");
                 exit(0);
@@ -51,13 +50,13 @@ int main(int argc, char *argv[])    //counting the number of variables
             {
                 printf("%.2f C\n", t);
                 printf("%.2f F\n", (t*9/5+32));
-        	    printf("%.2f K\n", (t+K0));
+        	    printf("%.2f K\n", (t+min[1]));
                 break;
             }
 		}
 		case 'F' :    //fahrenheit
 		{
-            if (t < -K1)
+            if (t < -min[2])
             {
                 printf("invalid input");
                 exit(0);
@@ -66,13 +65,13 @@ int main(int argc, char *argv[])    //counting the number of variables
             {
         	    printf("%.2f F\n", t);
         	    printf("%.2f C\n", ((t-32)*5/9));
-        	    printf("%.2f K\n", ((t+K1)*5/9));
+        	    printf("%.2f K\n", ((t+min[2])*5/9));
                 break;
             }
 		}
 		case 'K' :    //kelvin
 		{
-            if (t < 0)
+            if (t < min[0])
             {
                 printf("invalid input");
                 exit(0);
@@ -80,8 +79,8 @@ int main(int argc, char *argv[])    //counting the number of variables
             else
             {
         	    printf("%.2f K\n", t);
-        	    printf("%.2f C\n", (t-K0));
-                printf("%.2f F\n", (t*9/5-K1));
+        	    printf("%.2f C\n", (t-min[1]));
+                printf("%.2f F\n", (t*9/5-min[2]));
                 break;
             }
         }
@@ -89,24 +88,24 @@ int main(int argc, char *argv[])    //counting the number of variables
         {
         	printf("%.2f\n\n", t);
 
-            if (t < -K0)
+            if (t < -min[1])
                 printf("invalid input\n\n");
             else
             {
         	    printf("%.2f C:\n", t);
         	    printf("%.2f F\n", (t*9/5+32));
-        	    printf("%.2f K\n\n", (t+K0));
+        	    printf("%.2f K\n\n", (t+min[1]));
             }
-            if (t < -K1)
+            if (t < -min[2])
                 printf("invalid input\n\n");
             else
             {
         	    printf("%.2f F:\n", t);
         	    printf("%.2f C\n", ((t-32)*5/9));
-        	    printf("%.2f K\n\n", ((t+K1)*5/9));
+        	    printf("%.2f K\n\n", ((t+min[2])*5/9));
             }
 
-            if (t < 0)
+            if (t < min[0])
             {
                 printf("invalid input\n\n");
                 exit(0);
@@ -114,8 +113,8 @@ int main(int argc, char *argv[])    //counting the number of variables
             else
             {
         	    printf("%.2f K:\n", t);
-        	    printf("%.2f C\n", (t-K0));
-        	    printf("%.2f F\n", (t*9/5-K1));
+        	    printf("%.2f C\n", (t-min[1]));
+        	    printf("%.2f F\n", (t*9/5-min[2]));
             }
 		}
 
